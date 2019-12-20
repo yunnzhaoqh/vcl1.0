8ec760f53befdbf274fd7a467a1f4c02d05c93f4
wu-ze-peng <136320169@qq.comm>
2019-12-20 21:06:58 +0800

    修改冲突，登录逻辑已完成，未验证

---
+++ src/main/java/com/vcl/pojo/User.java
@@ -0,0 +1,76 @@
+package com.vcl.pojo;
+
+public class User {
+    private int id;
+    private String login_name;
+    private String password;
+    private String name;
+    private String gender;
+    private int age;
+    private String create_time;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin_name() {
+        return login_name;
+    }
+
+    public void setLogin_name(String login_name) {
+        this.login_name = login_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
