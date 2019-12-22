0a607e24a1e83a3d42e661c14f0da27c6253d7db
wu-ze-peng <136320169@qq.comm>
2019-12-22 13:56:54 +0800

    整合分页插件

--- src/main/java/com/vcl/pojo/PageResult.java
+++ src/main/java/com/vcl/pojo/PageResult.java
@@ -11,7 +11,53 @@ public class PageResult<T> {
     private int code;
     private String msg;
 
+    public Long getCount() {
+        return count;
+    }
 
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public Long getPage() {
+        return page;
+    }
+
+    public void setPage(Long page) {
+        this.page = page;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 
     public PageResult(Long count, List<T> data) {
         this.count = count;
